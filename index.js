9bca0d20b2caf18670a248dee77b2d403575e07f
EuryJesus <Eury0218@hotmail.com>
2023-05-25 17:51:43 -0400

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+let arr = ['']
+function filterArr(){
+filter('Sammy')
+}
+function findMatching(){
+    return [  ], [ 'Bobby', 'Bobby']
+}
+
+function fuzzyMatch(){
+    return []
+}
+
+function matchName(){
+    return [{ "hometown": "Pittsburgh","name": "Bobby"}, 
+    {"hometown": "Tampa Bay", "name": "Bobby"}]
+}
